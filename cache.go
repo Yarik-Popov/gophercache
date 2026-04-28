79fd3884a08ec3f7873914e07a801fdb530bdc73
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2026-04-28 18:45:45 -0400

    feat: add lock to cache

--- cache.go
+++ cache.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type Map[K comparable, V any] interface {
 	Get(key K) (V, bool)
@@ -14,6 +17,7 @@ type Cache[K comparable, V any] struct {
 	back        *nodeElement[K, V]
 	lookup      map[K]*nodeElement[K, V]
 	duration    time.Duration
+	lock        sync.Mutex
 }
 
 func CreateCache[K comparable, V any](maxElements uint32, duration time.Duration) *Cache[K, V] {
@@ -40,6 +44,9 @@ func CreateCache[K comparable, V any](maxElements uint32, duration time.Duration
 }
 
 func (c *Cache[K, V]) Get(key K) (V, bool) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
 	node, ok := c.lookup[key]
 	if !ok {
 		var zero V // Gets zeroed out so still has a value. Now code compiles
@@ -51,7 +58,7 @@ func (c *Cache[K, V]) Get(key K) (V, bool) {
 		panic("Node is expected but not found")
 	}
 
-	if c.duration != 0 {
+	if c.duration > 0 {
 		now := time.Now()
 		if node.expiryTime.Before(now) {
 			c.deleteNode(node)
@@ -67,6 +74,9 @@ func (c *Cache[K, V]) Get(key K) (V, bool) {
 }
 
 func (c *Cache[K, V]) Put(key K, value V) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
 	node, ok := c.lookup[key]
 	if ok {
 		node.value = value
@@ -77,17 +87,18 @@ func (c *Cache[K, V]) Put(key K, value V) {
 		return
 	}
 
+	newNode := &nodeElement[K, V]{
+		value:      value,
+		key:        key,
+		expiryTime: time.Now().Add(c.duration),
+	}
+
 	if c.maxElements == c.numElements {
 		oldLast := c.back.prev
 		c.deleteNode(oldLast)
 	}
 
 	c.numElements++
-	newNode := &nodeElement[K, V]{
-		value:      value,
-		key:        key,
-		expiryTime: time.Now().Add(c.duration),
-	}
 	c.insertNode(newNode)
 	c.lookup[key] = newNode
 }
