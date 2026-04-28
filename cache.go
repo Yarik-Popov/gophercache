2af729566c0ddd4064456f9957ac711d09434dc8
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2026-04-28 16:53:52 -0400

    chore: fix merge conflict

--- cache.go
+++ cache.go
@@ -1,14 +1,17 @@
 package main
 
+import "time"
+
 type Cache[K comparable, V any] struct {
 	maxElements uint32
 	numElements uint32
 	front       *nodeElement[K, V]
 	back        *nodeElement[K, V]
 	lookup      map[K]*nodeElement[K, V]
+	ttl         time.Duration
 }
 
-func CreateCache[K comparable, V any](maxElements uint32) *Cache[K, V] {
+func CreateCache[K comparable, V any](maxElements uint32, ttl time.Duration) *Cache[K, V] {
 	// Setup sentinel front and back nodes to make life easier when moving elements around
 	front := &nodeElement[K, V]{
 		prev: nil,
@@ -26,6 +29,7 @@ func CreateCache[K comparable, V any](maxElements uint32) *Cache[K, V] {
 		front:       front,
 		back:        back,
 		lookup:      make(map[K]*nodeElement[K, V]),
+		ttl:         ttl,
 	}
 	return cache
 }
@@ -41,6 +45,15 @@ func (c *Cache[K, V]) Get(key K) (V, bool) {
 		panic("Node is expected but not found")
 	}
 
+	if c.ttl != 0 {
+		now := time.Now()
+		if node.ttl.Before(now) {
+			c.deleteNode(node)
+			return node.value, false
+		}
+		node.ttl = now.Add(c.ttl)
+	}
+
 	node.removeNode()
 	c.insertNode(node)
 
@@ -51,6 +64,7 @@ func (c *Cache[K, V]) Put(key K, value V) {
 	node, ok := c.lookup[key]
 	if ok {
 		node.value = value
+		node.ttl = time.Now().Add(c.ttl)
 
 		node.removeNode()
 		c.insertNode(node)
@@ -59,15 +73,14 @@ func (c *Cache[K, V]) Put(key K, value V) {
 
 	if c.maxElements == c.numElements {
 		oldLast := c.back.prev
-		oldLast.removeNode()
-		c.numElements--
-		delete(c.lookup, oldLast.key)
+		c.deleteNode(oldLast)
 	}
 
 	c.numElements++
 	newNode := &nodeElement[K, V]{
 		value: value,
 		key:   key,
+		ttl:   time.Now().Add(c.ttl),
 	}
 	c.insertNode(newNode)
 	c.lookup[key] = newNode
@@ -80,6 +93,7 @@ type nodeElement[K comparable, V any] struct {
 	next  *nodeElement[K, V]
 	value V
 	key   K
+	ttl   time.Time
 }
 
 func (c *Cache[K, V]) insertNode(node *nodeElement[K, V]) {
@@ -88,6 +102,12 @@ func (c *Cache[K, V]) insertNode(node *nodeElement[K, V]) {
 	node.prev = c.front
 	node.next = oldFirst
 	oldFirst.prev = node
+}
+
+func (c *Cache[K, V]) deleteNode(node *nodeElement[K, V]) {
+	node.removeNode()
+	c.numElements--
+	delete(c.lookup, node.key)
 }
 
 func (node *nodeElement[K, V]) removeNode() {
